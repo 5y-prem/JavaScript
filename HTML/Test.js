bf2fa2b4f8fc2286a82c8ba1716e8e5bf0f6a511
5y-prem <151983696+5y-prem@users.noreply.github.com>
2023-12-02 09:21:59 +0530

    Launch.json

---
+++ HTML/Test.js
@@ -0,0 +1,8 @@
+const sandwich = ["peanut butter", "Jelly"]
+console.log(sandwich);
+
+const myArray = ["Hello Premanand", 40]
+console.log(myArray);
+
+const teams = [["RCB", 14], ["Sprite",45],]
+console.log(teams)
